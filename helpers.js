59c6a86c13e6f06f66166ccd30068e443bbaa54c
Andre <andrepaesbm@gmail.com>
2023-01-12 20:12:12 -0700

    added a urlsUser and getUserByEmail functions to keep the code DRY in the express.js file

--- helpers.js
+++ helpers.js
@@ -1,11 +1,23 @@
 const randomGenString = () => Math.random().toString(36).slice(2).substring(0, 6);
 
-const cookieHasUser = function(cookie, userDatabase) {
-  for (const user in userDatabase) {
-    if (cookie === user) {
-      return true;
+const getUserByEmail = (email, userDatabase) => {
+  for (let userId in userDatabase) {
+    if (userDatabase[userId].email === email) {
+      return userDatabase[userId];
     }
-  } return false;
+  }
+  return null;
 };
 
-module.exports = { cookieHasUser, randomGenString }; 
+const urlsUser = (id, urlDatabase) => {
+  const database = {};
+  for (const url in urlDatabase) {
+    if (urlDatabase[url].userId === id) {
+      database[url] = urlDatabase[url];
+    }
+  }
+  return database;
+};
+
+
+module.exports = { randomGenString, getUserByEmail, urlsUser };    
