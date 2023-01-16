37fc00bb96772afa992197788f85273b82e5e210
Andre <andrepaesbm@gmail.com>
2023-01-15 20:14:46 -0700

    Refactor the code, modify the routes as per request from the mentor, and improve the http requests depending on the error that the user is getting.

--- express_server.js
+++ express_server.js
@@ -43,11 +43,12 @@ const users = {};
 
 //Handles Login request
 app.get('/login', (req, res) => {
+  const loggedUser = req.session.user;
   const templateVars = {
-    user: users[req.session["userId"]],
+    user: users[loggedUser],
   }
   //Check if the user is already logged in and if not, redirect to the login page
-  if (req.session.userId) {
+  if (loggedUser) {
     return res.redirect("/urls");
   }
   res.render('urls_login', templateVars);
@@ -66,43 +67,45 @@ app.post('/login', (req, res) => {
   if (!bcrypt.compareSync(password, user.password)) {
     return res.status(403).send('Incorrect password');
   }
-  req.session.userId = user.id;
+  req.session.user = user.id;
   res.redirect('/urls');
 });
 
 //Handles Logout request
 app.post('/logout', (req, res) => {
-  res.clearCookie("session")
-  res.clearCookie("session.sig")
+  req.session = null;
   res.redirect('/login');
 });
 
 //Handles registration of new users
 app.get('/register', (req, res) => {
   const templateVars = {
-    user: users[req.session["userId"]]
+    user: users[req.session.user]
   };
+
+  //check if user is already logged in
+  if (req.session.user) {
+    return res.redirect('/urls');
+  }
   res.render('urls_register', templateVars);
 });
 
 app.post('/register', (req, res) => {
   const email = req.body.email;
-  const password = req.body.password;
+  const secretPassword = bcrypt.hashSync(req.body.password, 10);
+  const id = randomGenString(6);
+  const FoundUser = getUserByEmail(email, users)
+
 
   //check if user provided an email and password
-  if (!email || !password) {
+  if (!email || !secretPassword) {
     return res.status(400).send(`You must provide an email address and password to register! Try again to <a href=\"/Register\">register</a> to continue</body></html>`)
   };
-
-  const FoundUser = getUserByEmail(email, users)
 
   //Throw an 400 error if the user already exists
   if (FoundUser) {
     return res.status(400).send(`User with email ${email} already exists! Try again to <a href=\"/Register\">register</a> to continue</body></html>`)
   };
-
-  const id = randomGenString(6);
-  const secretPassword = bcrypt.hashSync(password, 10);
 
   const user = {
     id,
@@ -110,16 +113,16 @@ app.post('/register', (req, res) => {
     password: secretPassword
   };
 
-  users[id] = user;
+  users[user.id] = user;
   console.log(users);
-  req.session["userId"] = user.id;
+  req.session['user'] = user.id;
   res.redirect('/urls');
 });
 
 // If the user is already logged in or registered, redirect to the urls page
 app.get('/', (req, res) => {
-  const loggedUser = req.session.userId;
-  if (!loggedUser) {
+  const user = req.session.user;
+  if (!user) {
     res.redirect('/login');
   }
   res.redirect('/urls');
@@ -127,77 +130,81 @@ app.get('/', (req, res) => {
 
 //Reads all the URLS
 app.get("/urls", (req, res) => {
-  const userId = req.session["userId"];
-  const loggedUser = users[userId];
-  const urls = urlsUser(userId, urlDatabase);
+  const loggedUser = req.session.user;
+  const urls = urlsUser(loggedUser, urlDatabase);
+
+  //Check if the user is logged in and if not, throw an 401 error
+  if (!loggedUser) {
+    return res.status(401).send("<html><body>Please <a href=\"/login\">login</a> or <a href=\"/register\">register</a> to continue</body></html>\n");
+  }
 
   const templateVars = {
-    user: loggedUser,
+    user: users[loggedUser],
     urls: urls
   };
-  //Check if the user is logged in and if not, throw an 401 error
-  if (loggedUser) {
-    res.render("urls_index", templateVars);
-  } else {
-    return res.status(401).send("<html><body>Please <a href=\"/login\">login</a> or <a href=\"/register\">register</a> to continue</body></html>\n"
-    );
-  }
+
+  res.render("urls_index", templateVars);
 });
 
 //Creates a new URL if the user is logged in
 app.post("/urls", (req, res) => {
-  const userId = req.session.userId;
+  const loggedUser = req.session.user;
   const newUrl = randomGenString();
 
   //Throw an error if the user is not logged in
-  if (!userId) {
+  if (!loggedUser) {
     return res.status(401).send('You must <a href=\"/login\">login</a> to continue!');
   }
-  urlDatabase[newUrl] = { longURL: req.body.longURL, userId };
+  urlDatabase[newUrl] = { longURL: req.body.longURL, userId: loggedUser };
   res.redirect(`/urls/${newUrl}`);
 });
 
 
 //Creates new form
 app.get('/urls/new', (req, res) => {
-  const userId = req.session.userId;
-  const user = users[userId];
+  const loggedUser = req.session.user;
+  const user = users[loggedUser];
   const templateVars = { user };
 
   // if user is logged in, than render the urls_new template, else redirect to the login page
-  if (userId) {
-    res.render("urls_new", templateVars);
-  } else {
+  if (!loggedUser) {
     res.redirect("/login");
   }
+  res.render("urls_new", templateVars);
 });
 
 app.get('/urls/:id', (req, res) => {
-  const userId = req.session.userId;
+  const loggedUser = req.session.user;
   const shortURL = req.params.id;
+
+  //if the user is not logged in or registered, throw an error
+  if (!loggedUser) {
+    return res.status(401).send('<html><body>Please <a href="/login">login</a> or <a href="/register">register</a> to create or edit URLS!</body></html>');
+  }
+
+  //If user does not own the URL, display error message
+  if (urlDatabase[shortURL].userId !== loggedUser) {
+    return res.status(401).send('You unauthorized to view this URL!');
+  }
 
   //if URL does not exist, thrown an error
   if (!urlDatabase[shortURL]) {
     return res.status(404).send(`The selected URL ${shortURL} does not exist in the database`);
   }
-  //if the user is not logged in or registered, throw an error
-  if (!userId) {
-    return res.status(401).send('<html><body>Please <a href="/login">login</a> or <a href="/register">register</a> to create or edit URLS!</body></html>');
-  }
 
   const templateVars = {
-    user: users[userId],
+    user: users[loggedUser],
     id: shortURL,
     longURL: urlDatabase[shortURL].longURL
   };
-  
+
   res.render('urls_show', templateVars);
 });
 
 
 app.post("/urls/:id/", (req, res) => {
   const shortURL = req.params.id;
-  const userId = req.session.userId;
+  const user = req.session.user;
 
   //if the URL does not exist, thrown an error
   if (!urlDatabase[shortURL]) {
@@ -205,12 +212,12 @@ app.post("/urls/:id/", (req, res) => {
   }
 
   //If no user logged in, display error message
-  if (!userId) {
+  if (!user) {
     return res.status(401).send('<html><body>Please <a href="/login">login</a> or <a href="/register">register</a> to continue!</body></html>');
   }
   //If user does not own the URL, display error message
-  if (urlDatabase[shortURL].userId !== userId) {
-    return res.status(401).send('You cannot access this URL');
+  if (urlDatabase[shortURL].userId !== user) {
+    return res.status(401).send('You unauthorized to view this URL!');
   }
   urlDatabase[req.params.id].longURL = req.body.longURL;
   res.redirect('/urls');
@@ -229,20 +236,20 @@ app.get("/u/:id", (req, res) => {
 
 //Delete
 app.post("/urls/:id/delete", (req, res) => {
-  const user = req.session.userId;
+  const user = req.session.user;
   const shortURL = req.params.id;
 
   //if user is not logged in, thrown an error
   if (!user) {
-    return res.status(401).send("Please log in first!");
+    return res.status(401).send('<html><body>Please <a href="/login">login</a> or <a href="/register">register</a> to continue!</body></html>');
   }
   // if requested short url is not in the shortURLs array
   if (!urlDatabase[shortURL]) {
-    return res.send("short URL does not exist in the database!");
+    return res.status(404).send("short URL does not exist in the database!");
   }
   //if user does not own the URL, display an error message
   if (urlDatabase[shortURL].userId !== user) {
-    return res.send("You cannot view this URL");
+    return res.status(401).send('You unauthorized to view this URL!');
   }
   const shortURLId = req.params.id;
   delete urlDatabase[shortURLId];
